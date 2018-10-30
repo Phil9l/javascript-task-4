56e1e6f316e526a05ac9969959d979656a68d53b
phi191 <phi191@yandex-team.ru>
2018-10-30 23:49:21 +0500

    Init emit

--- emitter.js
+++ emitter.js
@@ -11,33 +11,104 @@ const isStar = true;
  * @returns {Object}
  */
 function getEmitter() {
+    const functions = new Map();
+
     return {
+
+        addEvent: function (event, context, handler, { times, frequency }) {
+            if (!functions.has(event)) {
+                functions.set(event, new Map());
+            }
+            if (!functions.get(event).has(context)) {
+                functions.get(event).set(context, []);
+            }
+            if (frequency <= 0) {
+                frequency = 1;
+            }
+            if (times <= 0) {
+                times = Infinity;
+            }
+            functions.get(event).get(context)
+                .push({
+                    handler: handler,
+                    times: times,
+                    frequency: frequency,
+                    count: 0
+                });
+        },
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {this}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            this.addEvent(event, context, handler, { times: 0, frequency: 0 });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {this}
          */
         off: function (event, context) {
-            console.info(event, context);
+            Array.from(functions.keys()).forEach(eventName => {
+                if (eventName === event || eventName.startsWith(event + '.')) {
+                    functions.get(eventName).delete(context);
+                }
+            });
+
+            return this;
+        },
+
+        handleEvent: function (context, handlerData) {
+            if (handlerData.count >= handlerData.times ||
+                handlerData.count % handlerData.frequency !== 0) {
+                handlerData.count++;
+
+                return;
+            }
+            handlerData.count++;
+            handlerData.handler.call(context);
+        },
+
+        handleEmit: function (event) {
+            if (!functions.has(event)) {
+                return;
+            }
+            Array.from(functions.get(event).entries()).forEach(([key, val]) => {
+                val.forEach(handlerData => {
+                    this.handleEvent(key, handlerData);
+                });
+            });
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {this}
          */
         emit: function (event) {
-            console.info(event);
+            let eventName = '';
+            let eventNames = [];
+            event.split('.').forEach(group => {
+                if (eventName !== '') {
+                    eventName += '.';
+                }
+                eventName += group;
+                eventNames.push(eventName);
+            });
+            eventNames.reverse();
+            eventNames.forEach(newEvent => {
+                this.handleEmit(newEvent);
+            });
+
+            return this;
         },
 
         /**
@@ -47,9 +118,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {this}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            this.addEvent(event, context, handler, { times: times, frequency: 0 });
+
+            return this;
         },
 
         /**
@@ -59,9 +133,12 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {this}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            this.addEvent(event, context, handler, { times: 0, frequency: frequency });
+
+            return this;
         }
     };
 }
